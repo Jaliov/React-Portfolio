55aa3346e62e0c2fa3c7ef1a9624ead7323fe113
Joel Rudin <jarud55@gmail.com>
2020-04-01 17:08:25 -0400

    tweaks for deploy

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import ShowBio from "./components/Bio";
 import ContactInfo from "./components/Contact";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-//import './components/Style.css';
-//import Portfolio from "./components/Portfolio";
 import NavElement from "./components/Home";
-//import Code_QuizImage from './images/Code_QuizImage.jpg'
-//import storefrontsColorizedTexture from "./components/images/storefrontsColorizedTexture.jpg"
 import "./App.css";
 
 class App extends Component {
