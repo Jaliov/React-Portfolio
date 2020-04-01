788c9b4550a6020856fcf065437047f20c371f63
Jaliov <50626351+Jaliov@users.noreply.github.com>
2020-04-01 19:43:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <Router>
+       basename={"/React-Portfolio"} 
         <div>
           <NavElement></NavElement>
           {/* <Route exact path="/" component = {}></Route> */}
