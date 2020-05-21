99b0c020e031ca7fc65d39c3df4ae332fcabfa9e
Joel Rudin <jarud55@gmail.com>
2020-05-21 18:13:06 -0400

    reload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line
-import React, { Component } from "react";
-import CardDisplay from "./components/Card";
-import ShowBio from "./components/Home";
-import ContactInfo from "./components/Contact";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavElement from "./components/NavBar";
-import "./App.css";
+import React, { Component } from 'react';
+import CardDisplay from './components/Card';
+import ShowBio from './components/Home';
+import ContactInfo from './components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavElement from './components/NavBar';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -15,11 +15,11 @@ class App extends Component {
         <div>
           <NavElement />
 
-          <Route exact path="/" component={ShowBio}></Route>
+          <Route exact path='/home' component={ShowBio}></Route>
 
-          <Route exact path="/portfolio" component={CardDisplay}></Route>
+          <Route exact path='/portfolio' component={CardDisplay}></Route>
 
-          <Route exact path="/contact" component={ContactInfo}></Route>
+          <Route exact path='/contact' component={ContactInfo}></Route>
         </div>
       </Router>
     );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,29 @@
 import React from 'react';
-
 import { Navbar, Nav } from 'react-bootstrap';
 
 const NavElement = () => {
   return (
-    <React.Fragment>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Navbar.Brand
-            id='Brand'
-            href='/'
-            style={{
-              fontWeight: 'bold',
-              backgroundColor: 'rgb(137, 224, 220)',
-            }}
-            className='text-light'
-          >
-            Joel Rudin
-          </Navbar.Brand>
-          <Nav className='mr-auto'>
-            <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='/portfolio'>Portfolio</Nav.Link>
-            <Nav.Link href='/contact'>Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </React.Fragment>
+    <Navbar bg='light' expand='lg'>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Navbar.Brand
+          id='brand'
+          href='/'
+          style={{
+            fontWeight: 'bold',
+            backgroundColor: 'rgb(137, 224, 220)',
+          }}
+          className='text-light'
+        >
+          Joel Rudin
+        </Navbar.Brand>
+        <Nav className='mr-auto'>
+          <Nav.Link href='/home'>Home</Nav.Link>
+          <Nav.Link href='/portfolio'>Portfolio</Nav.Link>
+          <Nav.Link href='/contact'>Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
