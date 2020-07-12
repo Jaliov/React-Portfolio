80355c7a755580bb0516608ba4d0d4b31b2b9daa
Joel Rudin <jarud55@gmail.com>
2020-07-12 16:20:11 -0400

    retore connect

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -53,7 +53,7 @@ class ContactInfo extends Component {
     console.log(payload);
 
     axios({
-      url: 'http://localhost:4000/testAPI/save',
+      url: '/testAPI/save',
       method: 'POST',
       data: payload,
     })
