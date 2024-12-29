1e42bc64fb2c00d9b9de8165804162ee844242b6
Joel Rudin <jarud55@gmail.com>
2024-12-29 15:11:20 -0500

    updates

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 
 const NavElement = () => {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-          <Link to='/home'>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Link to="/home">
             <h4>Joel Rudin</h4>
           </Link>
-        <Link className='nav-item nav-link active' to='/'>
-          Home
-        </Link>
-        <Link className='nav-item nav-link' to='/portfolio'>
-          Portfolio
-        </Link>
-        <Link className='nav-item nav-link' to='/contact'>
-          Contact
-        </Link>
+          <Link className="nav-item nav-link active" to="/">
+            Home
+          </Link>
+          <Link className="nav-item nav-link" to="/portfolio">
+            Portfolio
+          </Link>
+          <Link className="nav-item nav-link" to="/contact">
+            Contact
+          </Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
